19dc23c0078010c17a62d5a429bcdafbecd31c91
Tormod Fossum <tormod.fossum@nav.no>
2024-12-08 14:02:24 +0100

    Day 07 part 2

--- app/src/main/kotlin/dev/torcor/aoc/day/Day07.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day07.kt
@@ -1,47 +1,48 @@
 package dev.torcor.aoc.day
 
 import dev.torcor.aoc.utils.longsFrom
-import dev.torcor.aoc.utils.numbersFrom
 
 class Day07 : Day() {
-    //override val example = DAY_07_EXAMPLE
+    // override val example = DAY_07_EXAMPLE
+    private val plus = { a: Long, b: Long -> a + b }
+    private val multiply = { a: Long, b: Long -> a * b }
+    private val concat = { a: Long, b: Long -> "$a$b".toLong() }
 
-    override fun partOne(): AocResult = Solution(solvePartOne())
+    override fun partOne(): AocResult = Solution(solve(listOf(plus, multiply)))
+
+    override fun partTwo(): AocResult = Solution(solve(listOf(plus, multiply, concat)))
 
     private fun parse() = input.map {
-        val (test, values) = it.split(":").map { s -> longsFrom(s) }
-
-        test.first() to values
+        val (expected, values) = it.split(":").map { s -> longsFrom(s) }
+        expected.first() to values
     }
 
-    fun solvePartOne() = parse().map { test(it) }.sum()
+    private fun solve(operators: List<(Long, Long) -> Long>) = parse().sumOf { (expected, values) ->
+        test(expected, values, operators)
+    }
 
-    private fun test(it: Pair<Long, List<Long>>): Long {
-        val expected = it.first
-        val values = it.second
+    private fun test(
+        expected: Long,
+        values: List<Long>,
+        operators: List<(Long, Long) -> Long>,
+    ): Long {
+        val operatorCombinations = generateCombinations(values.size - 1, operators)
 
-        val operatorCombination = generateOperators(values.size - 1)
-
-        operatorCombination.forEach { combination ->
+        operatorCombinations.forEach { combination ->
             val result = values.reduceIndexed { index, acc, value ->
                 combination[index - 1](acc, value)
             }
 
-            if (result == expected) {
-                return expected
-            }
+            if (result == expected) return expected
         }
 
         return 0
     }
 
-    private fun generateOperators(n: Int): List<List<(Long, Long) -> Long>> {
-        val operators = listOf({ a: Long, b: Long -> a + b }, { a: Long, b: Long -> a * b })
-
-        return (1..n).fold(listOf(emptyList())) { acc, _ ->
+    private fun generateCombinations(n: Int, operators: List<(Long, Long) -> Long>): List<List<(Long, Long) -> Long>> =
+        (1..n).fold(listOf(emptyList())) { acc, _ ->
             acc.flatMap { combination -> operators.map { combination + it } }
         }
-    }
 }
 
 const val DAY_07_EXAMPLE = """
