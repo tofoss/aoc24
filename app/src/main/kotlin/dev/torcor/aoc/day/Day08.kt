0d23943053e0288f1b21b4ce8eb6e47895c0b61d
Tormod Fossum <tormod.fossum@nav.no>
2024-12-09 17:12:41 +0100

    Day 08 part 2

--- app/src/main/kotlin/dev/torcor/aoc/day/Day08.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day08.kt
@@ -6,45 +6,67 @@ class Day08 : Day() {
     // override val example: String = DAY_08_EXAMPLE
 
     override fun partOne(): AocResult = Solution {
-        findAntinodes().count()
+        solve(::antinodes)
     }
 
-    private val map = input.map { it.toList() }
+    override fun partTwo(): AocResult = Solution {
+        solve(::antinodes2)
+    }
 
-    private val frequencies =
-        map.flatMapIndexed { row, it ->
+    private val grid = input.map { it.toList() }
+
+    private val nodes =
+        grid.flatMapIndexed { row, it ->
             it.mapIndexedNotNull { col, freq ->
-                if (freq != '.') Frequency(Cell(row, col), freq) else null
+                if (freq != '.') Node(Cell(row, col), freq) else null
             }
         }
 
-    private val lines = frequencies.flatMapIndexed { i, f1 ->
-        frequencies.drop(i + 1).mapNotNull { f2 -> if (f2 != f1 && f2.frequency == f1.frequency) Pair(f1, f2) else null }
+    private val lines = nodes.flatMapIndexed { i, f1 ->
+        nodes.drop(i + 1).mapNotNull { f2 -> if (f2 != f1 && f2.frequency == f1.frequency) Pair(f1, f2) else null }
     }
 
-    private fun findAntinodes() = lines
-        .flatMap { antinodes(it) }
-        .filterNot { it.cell.isOutOfBounds(map) }
-        .distinct()
+    private fun solve(func: (Pair<Node, Node>) -> List<Node>) = lines
+        .flatMap(func)
+        .filterNot { it.cell.isOutOfBounds(grid) }
+        .distinctBy { it.cell }
+        .count()
 
-    private fun antinodes(line: Pair<Frequency, Frequency>): List<Frequency> {
+    private fun antinodes(line: Pair<Node, Node>): List<Node> {
         val (a, b) = line
 
         return listOf(antinode(a, b), antinode(b, a))
     }
 
-    fun antinode(a: Frequency, b: Frequency): Frequency {
+    fun antinode(a: Node, b: Node): Node {
         val rowMod = a.cell.row - b.cell.row
         val colMod = a.cell.col - b.cell.col
 
-        return Frequency(Cell(a.cell.row + rowMod, a.cell.col + colMod), '#')
+        return Node(Cell(a.cell.row + rowMod, a.cell.col + colMod), '#')
     }
-}
 
-data class Frequency(
-    val cell: Cell,
-    val frequency: Char,
-)
+    private fun antinodes2(line: Pair<Node, Node>): List<Node> {
+        val (a, b) = line
+
+        return line.toList() + antinodesUpdatedModel(a, b) + antinodesUpdatedModel(b, a)
+    }
+
+    fun antinodesUpdatedModel(a: Node, b: Node): Sequence<Node> {
+        val rowMod = a.cell.row - b.cell.row
+        val colMod = a.cell.col - b.cell.col
+
+        return generateSequence(a) { node ->
+            val cell = Cell(node.cell.row + rowMod, node.cell.col + colMod)
+
+            if (cell.isOutOfBounds(grid)) null else Node(cell, '#')
+        }
+    }
+
+    data class Node(
+        val cell: Cell,
+        val frequency: Char,
+    )
+}
 
 const val DAY_08_EXAMPLE = """
     ............
--- app/src/main/kotlin/dev/torcor/aoc/utils/Cell.kt
+++ app/src/main/kotlin/dev/torcor/aoc/utils/Cell.kt
@@ -4,9 +4,9 @@ data class Cell(
     val row: Int,
     val col: Int,
 ) {
-    fun <T> isOutOfBounds(matrix: List<List<T>>): Boolean {
-        val rows = matrix.size
-        val cols = matrix[0].size
+    fun <T> isOutOfBounds(grid: List<List<T>>): Boolean {
+        val rows = grid.size
+        val cols = grid[0].size
 
         return row !in 0..<rows || col !in 0..<cols
     }
