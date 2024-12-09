209a94a0bc1272f6f432a38ae31714437a16d265
Tormod Fossum <tormod.fossum@nav.no>
2024-12-09 22:47:58 +0100

    Day 09 part 2

--- app/src/main/kotlin/dev/torcor/aoc/day/Day09.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day09.kt
@@ -1,5 +1,7 @@
 package dev.torcor.aoc.day
 
+import java.util.LinkedList
+
 class Day09 : Day() {
     //override val example = "2333133121414131402"
 
@@ -7,14 +9,41 @@ class Day09 : Day() {
         checksum(fragment())
     }
 
+    override fun partTwo() = Solution {
+        checksum(moveWholeFiles().flatMap { file -> List(file.files) { file.index } + List(file.freeSpace) { null } })
+    }
+
     private val blocks = input.first().chunked(2).map { Pair(it[0].toString().toInt(), it.getOrNull(1)?.toString()?.toInt() ?: 0) }
+
+    private val files = input
+        .first()
+        .chunked(2)
+        .mapIndexed { index, it -> FileBlock(index, it[0].toString().toInt(), it.getOrNull(1)?.toString()?.toInt() ?: 0) }
 
     private val disk = blocks.flatMapIndexed { index, block ->
         List(block.first) { index } + List(block.second) { null }
     }
 
-    private fun checksum(disk: List<Int?>): Long {
-        return disk.filterNotNull().mapIndexed { index, i -> index * i.toLong() }.sum()
+    private fun checksum(disk: List<Int?>): Long = disk.mapIndexed { index, id -> index * (id?.toLong() ?: 0L) }.sum()
+
+    private fun moveWholeFiles(): List<FileBlock> {
+        val filelist = files.toCollection(LinkedList())
+
+        files.reversed().forEachIndexed { index, file ->
+            val freeFile = filelist.subList(0, filelist.size - index - 1).firstOrNull { it.freeSpace >= file.files }
+            if (freeFile != null) {
+                val freeIndex = filelist.indexOf(freeFile)
+                filelist[freeIndex] = freeFile.copy(freeSpace = 0)
+                filelist.add(freeIndex + 1, file.copy(freeSpace = freeFile.freeSpace - file.files))
+                val oldIndex = filelist.indexOfLast { it.index == file.index }
+                if (oldIndex != -1) {
+                    val oldFile = filelist[oldIndex]
+                    filelist[oldIndex] = oldFile.copy(files = 0, freeSpace = oldFile.freeSpace + oldFile.files)
+                }
+            }
+        }
+
+        return filelist
     }
 
     private fun fragment(): List<Int?> {
@@ -39,6 +68,12 @@ class Day09 : Day() {
 
         return mutableDisk
     }
+
+    data class FileBlock(
+        val index: Int,
+        val files: Int,
+        val freeSpace: Int,
+    )
 }
 
 fun main() = Day09().solve()
