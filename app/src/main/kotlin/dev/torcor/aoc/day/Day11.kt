f26e8eb33d37b6cdb90e4e964ece6f21c9e5ba4d
Tormod Fossum <tormod.fossum@nav.no>
2024-12-11 19:38:38 +0100

    Day 11 part 2

--- app/src/main/kotlin/dev/torcor/aoc/day/Day11.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day11.kt
@@ -1,29 +1,47 @@
 package dev.torcor.aoc.day
 
-import dev.torcor.aoc.utils.debug
-
 class Day11 : Day() {
-    //override val example: String = "125 17"
+    // override val example: String = "125 17"
 
-    override fun partOne() = Solution {
-        var stones = parse()
-        repeat(25) {
-            stones = blink(stones)
+    override fun partOne() = solve(25)
+
+    override fun partTwo() = solve(75)
+
+    private fun solve(n: Int) = Solution {
+        var stoneCounts = parse().groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+        repeat(n) {
+            stoneCounts = blink(stoneCounts)
         }
-        stones.size
+        stoneCounts.values.sum()
     }
 
-    fun parse() = input.first().split(" ").map { it.toLong() }
+    private fun parse(): List<Long> = input.first().split(" ").map { it.toLong() }
 
-    fun blink(stones: List<Long>): List<Long> = stones.flatMap {
-        if (it == 0L) {
-            listOf(1)
-        } else if (it.toString().length % 2 == 0) {
-            val str = it.toString()
-            listOf(str.substring(0, str.length / 2).toLong(), str.substring(str.length / 2, str.length).toLong())
-        } else {
-            listOf(it * 2024)
+    private fun blink(stoneCounts: Map<Long, Long>): Map<Long, Long> {
+        val nextCounts = mutableMapOf<Long, Long>()
+
+        for ((stone, count) in stoneCounts) {
+            when {
+                stone == 0L -> {
+                    nextCounts[1L] = nextCounts.getOrDefault(1L, 0L) + count
+                }
+
+                stone.toString().length % 2 == 0 -> {
+                    val str = stone.toString()
+                    val part1 = str.substring(0, str.length / 2).toLong()
+                    val part2 = str.substring(str.length / 2).toLong()
+                    nextCounts[part1] = nextCounts.getOrDefault(part1, 0L) + count
+                    nextCounts[part2] = nextCounts.getOrDefault(part2, 0L) + count
+                }
+
+                else -> {
+                    val newValue = stone * 2024
+                    nextCounts[newValue] = nextCounts.getOrDefault(newValue, 0L) + count
+                }
+            }
         }
+
+        return nextCounts
     }
 }
 
