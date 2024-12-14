92213e74782dee94d4e0432e8bc41c6b6fc3189c
Tormod Fossum <tormod.fossum@nav.no>
2024-12-14 10:51:36 +0100

    Day 13 cheated

--- app/src/main/kotlin/dev/torcor/aoc/day/Day13.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day13.kt
@@ -1,39 +1,57 @@
 package dev.torcor.aoc.day
 
-import dev.torcor.aoc.utils.debug
-import dev.torcor.aoc.utils.numbersFrom
+import dev.torcor.aoc.utils.longsFrom
 
 class Day13 : Day() {
-    override val example = DAY_13_EXAMPLE
+    // override val example = DAY_13_EXAMPLE
 
     override fun partOne() = Solution {
-        parse().sumOf { pressButtons(it) }
+        parse().sumOf { cramers(it) }
     }
 
-    private fun pressButtons(it: Triple<Pair<Int, Int>, Pair<Int, Int>, Pair<Int, Int>>): Int {
-        val ax = x(it.third, it.first, it.second)
-        val ay = y(it.third, it.first, it.second)
-
-        val bx = x(it.third, it.second, it.first)
-        val by = y(it.third, it.second, it.first)
-
-        val asect = ax.intersect(ay)
-        val bsect = bx.intersect(by)
-
-        if (asect.size != 1 || bsect.size != 1) {
-            it.debug("CASE -> ")
-            asect.debug("A -> ")
-            bsect.debug("B -> ")
+    override fun partTwo() = Solution {
+        parse().sumOf {
+            val priceModifier = 10000000000000L
+            val bigPrize = it.third.let { p -> p.copy(p.first + priceModifier, p.second + priceModifier) }
+            cramers(it.copy(third = bigPrize))
         }
-
-
-        return (asect.map { it * 3 }.firstOrNull() ?: 0) +
-            (bsect.map { it * 1 }.firstOrNull() ?: 0)
     }
 
+    /**
+     * Cramer's Rule
+     * Cramer's Rule is a method from linear algebra for solving systems of two linear equations with two unknowns (aa and bb) using determinants.
+     * It calculates aa and bb directly by dividing the determinant of the modified system by the determinant of the original coefficient matrix.
+     * It leverages the properties of determinants to find the unique solution (if it exists) to the system of equations.
+     *
+     * Recognizing a problem as a linear algebraic one with two unknowns is key. Look for these signs:
+     *     1. Two Equations: You have two equations that need to be satisfied simultaneously.
+     *     2. Two Variables: You’re solving for two unknowns (e.g., aa and bb).
+     *     3. Linear Relationship: Each equation is linear, meaning variables are multiplied by constants and summed.
+     *     4. Single Solution: The system can potentially be solved exactly, provided the determinant isn’t zero.
+     *
+     * https://www.reddit.com/r/adventofcode/comments/1hd7irq/2024_day_13_an_explanation_of_the_mathematics/
+     */
+    private fun cramers(equation: Triple<Pair<Long, Long>, Pair<Long, Long>, Pair<Long, Long>>): Long {
+        val (buttonA, buttonB, prize) = equation
+
+        val a =
+            (prize.first * buttonB.second - prize.second * buttonB.first) /
+                (buttonA.first * buttonB.second - buttonA.second * buttonB.first)
+        val b =
+            (buttonA.first * prize.second - buttonA.second * prize.first) /
+                (buttonA.first * buttonB.second - buttonA.second * buttonB.first)
+
+        return if (buttonA.first * a + buttonB.first * b == prize.first &&
+            buttonA.second * a + buttonB.second * b == prize.second
+        ) {
+            a * 3L + b
+        } else {
+            0
+        }
+    }
 
     fun parse() = inputRaw.trim().split("\n\n").map {
-        val numbers = it.split("\n").map { numbersFrom(it) }
+        val numbers = it.split("\n").map { longsFrom(it) }
         Triple(
             Pair(numbers[0][0], numbers[0][1]),
             Pair(numbers[1][0], numbers[1][1]),
@@ -60,59 +78,4 @@ const val DAY_13_EXAMPLE = """
     Prize: X=18641, Y=10279 
 """
 
-fun main() {
-    /*
-     val prize = Pair(7870, 6450)
-     val a = Pair(17, 86)
-     val b = Pair(84, 37)
-     */
-    foo(Pair(8400, 5400), Pair(94, 34), Pair(22, 67))
-    foo(Pair(7870, 6450), Pair(17, 86), Pair(84, 37))
-
-    println("SHOULD NOT CONTAIN MATCHES:")
-    foo(Pair(12784, 12176), Pair(26, 66), Pair(67, 21))
-    foo(Pair(18641, 10279), Pair(69, 23), Pair(27, 71))
-
-    Day13().solve()
-}
-
-private fun foo(
-    prize: Pair<Int, Int>,
-    a: Pair<Int, Int>,
-    b: Pair<Int, Int>,
-) {
-    prize.debug()
-    x(prize, a, b).debug("ax -> ")
-    y(prize, a, b).debug("ay -> ")
-    println()
-    x(prize, b, a).debug("bx -> ")
-    y(prize, b, a).debug("by -> ")
-    println()
-    println()
-}
-
-private fun x(
-    prize: Pair<Int, Int>,
-    a: Pair<Int, Int>,
-    b: Pair<Int, Int>,
-) = (1..100)
-    .mapNotNull { i ->
-        if ((prize.first - a.first * i) % b.first == 0) {
-            i
-        } else {
-            null
-        }
-    }.toSet()
-
-private fun y(
-    prize: Pair<Int, Int>,
-    a: Pair<Int, Int>,
-    b: Pair<Int, Int>,
-) = (1..100)
-    .mapNotNull { i ->
-        if ((prize.second - a.second * i) % b.second == 0) {
-            i
-        } else {
-            null
-        }
-    }.toSet()
+fun main() = Day13().solve()
