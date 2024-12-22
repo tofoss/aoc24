927fbbb50076d9863d4f49d6a045395d29040429
Tormod Fossum <tormod.fossum@nav.no>
2024-12-22 10:40:26 +0100

    Day 22 part 2

--- app/src/main/kotlin/dev/torcor/aoc/day/Day22.kt
+++ app/src/main/kotlin/dev/torcor/aoc/day/Day22.kt
@@ -1,10 +1,29 @@
 package dev.torcor.aoc.day
 
 class Day22 : Day() {
-    // override val example = DAY_22_EXAMPLE
+    //override val example = DAY_22_EXAMPLE
 
     override fun partOne() = Solution {
-        parse().sumOf { it.secretNumber2000() }
+        parse().sumOf { it.secretNumbers().last() }
+    }
+
+    override fun partTwo() = Solution {
+        val priceSequences = parse().map {
+            it
+                .secretNumbers()
+                .map { n -> n.price() }
+                .sequences()
+        }
+
+        val sequenceValues = mutableMapOf<List<Long>, Long>()
+
+        priceSequences.forEach { seq ->
+            seq.forEach { (k, v) ->
+                sequenceValues.merge(k, v) { oldValue, newValue -> oldValue + newValue }
+            }
+        }
+
+        sequenceValues.maxBy { it.value }
     }
 
     private val steps = listOf(
@@ -13,20 +32,40 @@ class Day22 : Day() {
         { n: Long -> (n * 2048L).mixAndPrune(n) },
     )
 
-    private fun Long.secretNumber2000(): Long = (1..2000).fold(this) { acc, _ -> acc.secretNumber() }
+    private fun Long.secretNumbers() = generateSequence(this) { it.secretNumber() }.take(2001)
 
     private fun Long.secretNumber(): Long = steps.fold(this) { acc, fn -> fn(acc) }
 
     private fun Long.mixAndPrune(other: Long) = this.xor(other) % 16777216L
 
+    private fun Long.price() = this % 10
+
+    private fun Long.change(previous: Long) = this - previous
+
+    private fun Sequence<Long>.sequences(): MutableMap<List<Long>, Long> {
+        val indexed = this.withIndex()
+        val prices = this.toList()
+
+        val unique = mutableMapOf<List<Long>, Long>()
+
+        indexed.drop(1).windowed(4).forEach {
+            val seq = it.map { (index, n) -> n.change(prices[index - 1]) }
+            if (unique[seq] == null) {
+                unique[seq] = it.last().value
+            }
+        }
+
+        return unique
+    }
+
     private fun parse() = input.map { it.toLong() }
 }
 
 const val DAY_22_EXAMPLE = """
-    1
-    10
-    100
-    2024
+1
+2
+3
+2024
 """
 
 fun main() = Day22().solve()
